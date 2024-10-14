61321542da68c1b96fc2c87daf90e2db089568c6
Elvis Durić <duric_elvis@live.com>
2024-10-14 10:41:38 +0200

    Update authorization.ps1

--- scripts/authorization.ps1
+++ scripts/authorization.ps1
@@ -1,6 +1,6 @@
 # Provide User Credentials
-$username = "safetybuildserver"
-$password = "Buildserver2023+=21"
+$username = '$env:USERNAME'
+$password = '$env:PASSWORD'
 
 # Base64 encode the credentials
 $base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
